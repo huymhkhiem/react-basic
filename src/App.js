8183b30921ade2ebd9cd3b7c77722cb412c1cdf6
ZenKoi <khiemb1505831@gmail.com>
2023-02-23 15:11:15 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hôm nay tại sao tôi buồn
         </p>
         <a
           className="App-link"
